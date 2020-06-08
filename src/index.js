4b8b2da08866ce4a9e6ea6dff602e70cb9dabc00
z3r0 <raul.ma.tr@gmail.com>
2020-06-08 19:20:23 +0200

    first Commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+const dotenv = require('dotenv');
+const axios = require('axios');
+dotenv.config();
+
+const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+app.get('/', (req, res) => {
+    res.send('backPong server is running');
+});
+
+//socket io logic
+io.on('connection', socket => {
+
+    console.log('User connected');
+
+    // Nos conectamos a una room llamada game
+    socket.on('start',()=>{
+        socket.join('game');
+    })
+
+    // Emitimos a todos los players de game
+    socket.on('down',(data)=> {
+        socket.to('game').emit('down', data);
+    })
+    
+    socket.on('move',(data)=> {
+        socket.to('game').emit('move', data);
+    })
+
+});
+
+const PORT = process.env.PORT || 5050;
+
+server.listen(PORT, () => {
+    console.log('running on ' + PORT);
+});
