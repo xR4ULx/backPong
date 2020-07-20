f43a3c044638bc93d574894997e176b921d16ceb
RaulMacias <raul.ma.tr@gmail.com>
2020-07-20 13:38:42 +0200

    ondisconnect

--- src/index.js
+++ src/index.js
@@ -3,17 +3,18 @@ const http = require('http');
 const socketio = require('socket.io');
 const dotenv = require('dotenv');
 const { json, response } = require('express');
+const { disconnect } = require('process');
 dotenv.config();
 
 const app = express();
 const server = http.Server(app);
 const io = socketio(server);
 
-class Player{
-    constructor(displayName, socket_id, room){
-       this.displayName = displayName ;
-       this.socket_id = socket_id;
-       this.room = room;
+class Player {
+    constructor(displayName, socket_id, room) {
+        this.displayName = displayName;
+        this.socket_id = socket_id;
+        this.room = room;
     }
 }
 
@@ -24,7 +25,7 @@ function getPlayerByName(displayName) {
         player = Players.findIndex(item => item.displayName === displayName);
         if (player != -1) {
             return Players[player];
-        }else{
+        } else {
             return null;
         }
     } catch (error) {
@@ -39,7 +40,7 @@ function getPlayerById(id) {
         player = Players.findIndex(item => item.socket_id === id);
         if (player != -1) {
             return Players[player];
-        }else{
+        } else {
             return null;
         }
     } catch (error) {
@@ -50,6 +51,7 @@ function getPlayerById(id) {
 };
 
 
+
 app.get('/', (req, res) => {
     res.json(Players);
 });
@@ -58,39 +60,54 @@ app.get('/', (req, res) => {
 io.on('connection', socket => {
 
     socket.emit('on-connected');
-    
-    socket.on('login',(displayName)=>{
+
+    socket.on('disconnect', () => {
+        try {
+            player = getPlayerById(socket.id);
+            io.to(socket.id).emit('on-finish', data);
+            socket.in(player.room).emit('on-finish', data);
+            if (player.room != null) {
+                socket.leave(player.room);
+                player.room = null;
+            }
+            console.log(player.displayName + 'disconnect');
+        } catch (error) {
+            console.log(error.message);
+        }
+    });
+
+    socket.on('login', (displayName) => {
         indexPlayer = Players.findIndex(item => item.displayName === displayName);
-        if(indexPlayer != -1){
-            Players.splice(indexPlayer,1);
-            p = new Player(displayName, socket.id,null);
+        if (indexPlayer != -1) {
+            Players.splice(indexPlayer, 1);
+            p = new Player(displayName, socket.id, null);
             Players.push(p);
-        }else{
-            p = new Player(displayName, socket.id,null);
+        } else {
+            p = new Player(displayName, socket.id, null);
             Players.push(p);
         }
     })
 
-    socket.on('logout', (displayName)=>{
+    socket.on('logout', (displayName) => {
         player = getPlayerByName(displayName);
 
         /* Salimos de la room si existe */
-        if(player.room != null){
+        if (player.room != null) {
             socket.leave(player.room);
             player.room = null;
         }
         /* Eliminamos el usuario de la lista */
-        if(player != null){
-            Players.splice(player,1);
+        if (player != null) {
+            Players.splice(player, 1);
         }
     })
 
-    socket.on('request',(displayName)=>{
-        
+    socket.on('request', (displayName) => {
+
         player1 = getPlayerById(socket.id);
         player2 = getPlayerByName(displayName);
 
-        if(player1 != null && player2 != null){
+        if (player1 != null && player2 != null) {
 
             roomid = player1.socket_id + player2.socket_id;
             player1.room = roomid;
@@ -99,70 +116,70 @@ io.on('connection', socket => {
             io.sockets.connected[player2.socket_id].join(roomid);
 
             io.to(player2.socket_id).emit('on-request', player1.displayName);
-        }else{
+        } else {
             io.to(socket.id).emit('on-cancel-request', true);
         }
 
     })
 
-    socket.on('response',({ displayName, accept })=>{
+    socket.on('response', ({ displayName, accept }) => {
 
-        if(accept){
+        if (accept) {
 
             player1 = getPlayerByName(displayName);
             player2 = getPlayerById(socket.id);
 
-            if(player1 != null && player2 != null){
+            if (player1 != null && player2 != null) {
 
                 io.to(player1.socket_id).emit('on-response', true);
 
-            }else{
+            } else {
                 io.to(socket.id).emit('on-cancel-request', true);
             }
 
-        }else{
+        } else {
             io.to(player1.socket_id).emit('on-response', false);
         }
     })
-    
+
 
 
     // Emitimos a todos los players de game
-    socket.on('changeTurn',(data)=> {
+    socket.on('changeTurn', (data) => {
         try {
             player = getPlayerById(socket.id);
-            socket.in(player.room).emit('on-changeTurn',data);
+            socket.in(player.room).emit('on-changeTurn', data);
         } catch (error) {
             console.log(error.message);
         }
-        
+
     })
 
-    socket.on('finish', (data)=>{
+    socket.on('finish', (data) => {
         try {
             player = getPlayerById(socket.id);
-            io.to(socket.id).emit('on-finish',data);
-            socket.in(player.room).emit('on-finish',data);
+            io.to(socket.id).emit('on-finish', data);
+            socket.in(player.room).emit('on-finish', data);
             console.log(player.displayName + 'finish');
         } catch (error) {
             console.log(error.message);
         }
-        
+
     })
 
-    socket.on('exit-game',(data)=>{
+    socket.on('exit-game', (data) => {
         try {
             player = getPlayerById(socket.id);
-            io.to(socket.id).emit('on-exit-game', true);
-            if(player.room != null){
+            if (player.room != null) {
                 socket.leave(player.room);
                 player.room = null;
             }
+            io.to(socket.id).emit('on-exit-game', true);
             console.log(player.displayName + 'exit-game');
         } catch (error) {
             console.log(error.message);
         }
-        
+
     })
 
 });
