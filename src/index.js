c68c249ae300557a32ae40f8e27b019c33277a9a
RaulMacias <raul.ma.tr@gmail.com>
2020-08-01 10:51:34 +0200

    changes_prod

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ function getPlayerByName(displayName) {
             return null;
         }
     } catch (error) {
-        console.log(error.message);
+        console.log('wizard say:' + error.message);
         return null;
     }
 
@@ -44,7 +44,7 @@ function getPlayerById(id) {
             return null;
         }
     } catch (error) {
-        console.log(error.message);
+        console.log('wizard say:' + error.message);
         return null;
     }
 
@@ -72,89 +72,116 @@ io.on('connection', socket => {
             }
             console.log(player.displayName + 'disconnect');
         } catch (error) {
-            console.log(error.message);
+            console.log('wizard say:' + error.message);
         }
     });
 
     socket.on('login', (displayName) => {
-        indexPlayer = Players.findIndex(item => item.displayName === displayName);
-        if (indexPlayer != -1) {
-            Players.splice(indexPlayer, 1);
-            p = new Player(displayName, socket.id, null);
-            Players.push(p);
-        } else {
-            p = new Player(displayName, socket.id, null);
-            Players.push(p);
+
+        try {
+            indexPlayer = Players.findIndex(item => item.displayName === displayName);
+            if (indexPlayer != -1) {
+                Players.splice(indexPlayer, 1);
+                p = new Player(displayName, socket.id, null);
+                Players.push(p);
+            } else {
+                p = new Player(displayName, socket.id, null);
+                Players.push(p);
+            }
+        } catch (error) {
+            console.log('wizard say:' + error.message);
         }
+
     })
 
     socket.on('logout', (displayName) => {
-        player = getPlayerByName(displayName);
 
-        /* Salimos de la room si existe */
-        if (player.room != null) {
-            socket.leave(player.room);
-            player.room = null;
+        try {
+            player = getPlayerByName(displayName);
+
+            if (player != null) {
+                /* Salimos de la room si existe */
+                if (player.room != null) {
+                    socket.leave(player.room);
+                    player.room = null;
+                }
+                /* Eliminamos el usuario de la lista */
+                if (player != null) {
+                    Players.splice(player, 1);
+                }
+            } else {
+                console.log('wizard say: Player null');
+            }
+
+        } catch (error) {
+            console.log('wizard say:' + error.message);
         }
-        /* Eliminamos el usuario de la lista */
-        if (player != null) {
-            Players.splice(player, 1);
-        }
+
     })
 
     socket.on('request', (displayName) => {
+        try {
+            player1 = getPlayerById(socket.id);
+            player2 = getPlayerByName(displayName);
 
-        player1 = getPlayerById(socket.id);
-        player2 = getPlayerByName(displayName);
+            if (player1 != null && player2 != null) {
 
-        if (player1 != null && player2 != null) {
+                roomid = player1.socket_id + player2.socket_id;
+                player1.room = roomid;
+                player2.room = roomid;
+                io.sockets.connected[player1.socket_id].join(roomid);
+                io.sockets.connected[player2.socket_id].join(roomid);
 
-            roomid = player1.socket_id + player2.socket_id;
-            player1.room = roomid;
-            player2.room = roomid;
-            io.sockets.connected[player1.socket_id].join(roomid);
-            io.sockets.connected[player2.socket_id].join(roomid);
-
-            io.to(player2.socket_id).emit('on-request', player1.displayName);
-        } else {
+                io.to(player2.socket_id).emit('on-request', player1.displayName);
+            } else {
+                io.to(socket.id).emit('on-cancel-request', true);
+            }
+        } catch (error) {
             io.to(socket.id).emit('on-cancel-request', true);
+            console.log('wizard say:' + error.message);
         }
+
 
     })
 
     socket.on('response', ({ displayName, accept }) => {
+        try {
+            if (accept) {
 
-        if (accept) {
+                player1 = getPlayerByName(displayName);
+                player2 = getPlayerById(socket.id);
 
-            player1 = getPlayerByName(displayName);
-            player2 = getPlayerById(socket.id);
+                if (player1 != null && player2 != null) {
 
-            if (player1 != null && player2 != null) {
+                    io.to(player1.socket_id).emit('on-response', true);
 
-                io.to(player1.socket_id).emit('on-response', true);
+                } else {
+                    io.to(socket.id).emit('on-cancel-request', true);
+                }
 
             } else {
-                io.to(socket.id).emit('on-cancel-request', true);
+                io.to(player1.socket_id).emit('on-response', false);
             }
-
-        } else {
-            io.to(player1.socket_id).emit('on-response', false);
+        } catch (error) {
+            io.to(socket.id).emit('on-cancel-request', true);
+            console.log('wizard say:' + error.message);
         }
+
     })
 
 
 
     // Emitimos a todos los players de game
     socket.on('changeTurn', (data) => {
-            try {
-                player = getPlayerById(socket.id);
-                socket.in(player.room).emit('on-changeTurn', data);
-            } catch (error) {
-                console.log(error.message);
-            }
+        try {
+            player = getPlayerById(socket.id);
+            socket.in(player.room).emit('on-changeTurn', data);
+        } catch (error) {
+            console.log('wizard say:' + error.message);
+        }
 
-        })
-        //Coment
+    });
+
 
     socket.on('finish', (data) => {
         try {
@@ -162,10 +189,10 @@ io.on('connection', socket => {
             socket.in(player.room).emit('on-finish', data);
             console.log(player.displayName + 'finish');
         } catch (error) {
-            console.log(error.message);
+            console.log('wizard say:' + error.message);
         }
 
-    })
+    });
 
     socket.on('exit-game', (data) => {
         try {
@@ -177,7 +204,7 @@ io.on('connection', socket => {
             io.to(socket.id).emit('on-exit-game', true);
             console.log(player.displayName + 'exit-game');
         } catch (error) {
-            console.log(error.message);
+            console.log('wizard say:' + error.message);
         }
 
     })
